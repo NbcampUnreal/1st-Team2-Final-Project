91c9808b9ca6d1d556e6024d3503c8543ba4caf7
rkddus96 <103873698+rkddus96@users.noreply.github.com>
2025-05-07 21:16:54 +0900

    🎉 add Framework파일 생성

---
+++ Source/AbyssDiverUnderWorld/Framework/ADInGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ADInGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABYSSDIVERUNDERWORLD_API AADInGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
