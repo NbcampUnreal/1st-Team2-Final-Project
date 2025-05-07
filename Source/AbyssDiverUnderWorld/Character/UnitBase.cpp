2a8285900edaf69e4593414de59b95a86b7eed09
handakim25 <handakim25@gmail.com>
2025-05-07 17:21:53 +0900

    Add: UnitBase, StatComponent 초기 클래스 생성
    UnitBase, StatComponent 초기값만 생성했습니다.

---
+++ Source/AbyssDiverUnderWorld/Character/UnitBase.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UnitBase.h"
+
+#include "StatComponent.h"
+
+AUnitBase::AUnitBase()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+
+	StatComponent = CreateDefaultSubobject<UStatComponent>(TEXT("StatComponent"));
+}
+
+void AUnitBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+float AUnitBase::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent,
+	class AController* EventInstigator, AActor* DamageCauser)
+{
+	float Damage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	StatComponent->TakeDamage(Damage);
+	
+	return Damage;
+}
