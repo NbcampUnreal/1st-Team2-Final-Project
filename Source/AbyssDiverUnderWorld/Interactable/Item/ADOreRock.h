ba441a01214931b00dcfc4fbdc5ba1a3de18e972
stone1397 <157870045+stone1397@users.noreply.github.com>
2025-05-07 22:26:42 +0900

    🎉 add
    구조체를 넣어둘 FStructContainer 추가
    ADItemBase, ADOreRock, IADInteractable DT_DropEntry 추가

--- Source/AbyssDiverUnderWorld/Interactable/Item/ADOreRock.h
+++ Source/AbyssDiverUnderWorld/Interactable/Item/ADOreRock.h
@@ -1,13 +1,35 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Interface/IADInteractable.h"
+#include "Engine/StreamableManager.h"
+#include "Engine/AssetManager.h"
 #include "ADOreRock.generated.h"
 
+class AADItemBase;
+
+USTRUCT(BlueprintType)
+struct FDropEntry : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SpawnWeight = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftClassPtr<AADItemBase> ItemClass;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	uint8 MinCount = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	uint8 MaxCount = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MinMass = 5.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MaxMass = 15.f;
+};
+
 UCLASS()
-class ABYSSDIVERUNDERWORLD_API AADOreRock : public AActor
+class ABYSSDIVERUNDERWORLD_API AADOreRock : public AActor, public IIADInteractable
 {
 	GENERATED_BODY()
 	
@@ -19,8 +41,49 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+#pragma region Method
+public:
+	virtual void Interact(AActor* InstigatorActor) override;
+	
+	void HandleMineRequest(APawn* InstigatorPawn);
 
+	void SpawnDrops();
+
+	void OnAssetLoaded(FDropEntry* Entry, float Mass);
+
+protected:
+	UFUNCTION()
+	void OnRep_RemainingMines();
+
+private:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+#pragma endregion
+
+#pragma region Variable
+public:
+	UPROPERTY(ReplicatedUsing=OnRep_RemainingMines, EditAnywhere, Category = "Mining")
+	int32 RemainingMines = 3;
+	UPROPERTY(EditDefaultsOnly, Category = "Drops")
+	UDataTable* DropTable;
+	UPROPERTY(EditDefaultsOnly, Category = "Mining")
+	UParticleSystem* FractureFX;
+	UPROPERTY(EditDefaultsOnly, Category = "Mining")
+	TObjectPtr<USoundBase> FractureSound;
+
+protected:
+
+
+private:
+	TArray<FDropEntry*> CachedEntries;
+	TArray<float>  CumulativeWeights;
+	float TotalWeight = 0.f;
+	FStreamableManager Streamable;
+
+#pragma endregion
+
+#pragma region Getter, Setteer
+public:
+
+#pragma endregion
 };
