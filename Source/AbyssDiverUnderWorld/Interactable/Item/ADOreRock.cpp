1bc3b1c269e486839fd7d182532cd2890ebba52f
stone1397 <157870045+stone1397@users.noreply.github.com>
2025-05-07 16:36:43 +0900

    🎉 add ADOreRock, ✨ update Build.cs
    Interactable/Item 폴더에 ADOreRock C++ 파일 추가했습니다.
    파일 경로 찾을 수 있도록 수정했습니다.

---
+++ Source/AbyssDiverUnderWorld/Interactable/Item/ADOreRock.cpp
@@ -0,0 +1,24 @@
+#include "Interactable/Item/ADOreRock.h"
+
+// Sets default values
+AADOreRock::AADOreRock()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AADOreRock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AADOreRock::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
