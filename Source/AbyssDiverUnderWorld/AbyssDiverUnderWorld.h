fc7f0d610b102d05f34b977ba52e3fe4ec2c80f7
meo-young <meoyoung000@naver.com>
2025-05-07 16:31:18 +0900

    [Update] 커스텀 로그 이니셜 변경

--- Source/AbyssDiverUnderWorld/AbyssDiverUnderWorld.h
+++ Source/AbyssDiverUnderWorld/AbyssDiverUnderWorld.h
@@ -6,7 +6,7 @@
 
 #define LOG_NETMODEINFO ((GetNetMode() == NM_Client) ? *FString::Printf(TEXT("Client%d"), UE::GetPlayInEditorID()) : ((GetNetMode() == ENetMode::NM_Standalone) ? TEXT("STANDLALONE") : TEXT("SERVER")))
 #define LOG_CALLINFO ANSI_TO_TCHAR(__FUNCTION__)
-#define LOG(Format, ...) UE_LOG(AbyssDiver, Warning, TEXT("[%s] %s %s"), LOG_NETMODEINFO, LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__));
-#define LOG2(Format, ...) UE_LOG(AbyssDiver, Warning, TEXT("%s %s"), LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__));
+#define LOGN(Format, ...) UE_LOG(AbyssDiver, Warning, TEXT("[%s] %s %s"), LOG_NETMODEINFO, LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__));
+#define LOG(Format, ...) UE_LOG(AbyssDiver, Warning, TEXT("%s %s"), LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__));
 
 DECLARE_LOG_CATEGORY_EXTERN(AbyssDiver, Warning, All);
