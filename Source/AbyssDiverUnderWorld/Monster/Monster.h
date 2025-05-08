dcaa86c380e4ad4818a3e5f66f7039119d3681d6
JungHyaechang <hch9097@gmail.com>
2025-05-08 14:33:43 +0900

    [Add] Monster(base) Character, AIController 클래스 생성, Tentacle Character, AIController 생성

---
+++ Source/AbyssDiverUnderWorld/Monster/Monster.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/UnitBase.h"
+#include "Monster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABYSSDIVERUNDERWORLD_API AMonster : public AUnitBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/AbyssDiverUnderWorld/Monster/Tentacle/TentacleAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Monster/MonsterAIController.h"
+#include "TentacleAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABYSSDIVERUNDERWORLD_API ATentacleAIController : public AMonsterAIController
+{
+	GENERATED_BODY()
+	
+};
