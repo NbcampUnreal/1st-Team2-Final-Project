bf369582416f0c48cc563b82873a78ebb85fbbe3
erldfd <hseongrae@gmail.com>
2025-05-08 20:50:40 +0900

    🎉 add Shop Widgets, RichTextTable, Shop 관련 클래스들, ✨ update 로그 매크로 추가
    ### Shop
    - 상점 기능 기초 작업 완료
    - 상점 위젯 생성만 해둠
    - RichText 사용을 위한 테이블 추가
    - 필요없는 Material 삭제
    
    ### 로그 매크로 추가
    - LOGV : Verbosity를 넣을 수 있는 LOG
    - LOGVN : Verbosity를 넣을 수 있는 LOGN

---
+++ Source/AbyssDiverUnderWorld/Shops/ShopButton.cpp
@@ -0,0 +1,21 @@
+#include "Shops/ShopButton.h"
+
+
+AShopButton::AShopButton()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AShopButton::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AShopButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
