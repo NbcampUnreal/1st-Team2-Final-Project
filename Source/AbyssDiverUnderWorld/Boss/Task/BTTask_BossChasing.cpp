4db51c5382d6f7dedce1fdcac250f2b3f45aad28
meo-young <meoyoung000@naver.com>
2025-05-08 19:22:47 +0900

    [Add] 보스 추적 테스크 구현

---
+++ Source/AbyssDiverUnderWorld/Boss/Task/BTTask_BossChasing.cpp
@@ -0,0 +1,64 @@
+#include "Boss/Task/BTTask_BossChasing.h"
+#include "AbyssDiverUnderWorld.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Boss/Boss.h"
+#include "Boss/EBossState.h"
+
+const FName UBTTask_BossChasing::BossStateKey = "BossState";
+
+UBTTask_BossChasing::UBTTask_BossChasing()
+{
+	NodeName = TEXT("Boss Chasing");
+	bNotifyTick = true;
+	bTickIntervals = true;
+}
+
+EBTNodeResult::Type UBTTask_BossChasing::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if (!IsValid(AIController)) return EBTNodeResult::Failed;
+
+	ACharacter* Character = AIController->GetCharacter();
+	if (!IsValid(Character)) return EBTNodeResult::Failed;
+
+	ABoss* Boss = Cast<ABoss>(Character);
+	if (!IsValid(Boss)) return EBTNodeResult::Failed;
+
+	Boss->M_PlayAnimation(Boss->MoveAnimation);
+	Boss->SetMoveSpeed(MoveSpeedMultiplier);
+	
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_BossChasing::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+	
+	ABoss* Boss = Cast<ABoss>(OwnerComp.GetAIOwner()->GetCharacter());
+	if (!IsValid(Boss))
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+		return;
+	}
+
+	if (AccumulatedTime >= TickInterval)
+	{
+		Boss->MoveToTarget();
+		AccumulatedTime = 0.0f;
+	}
+	else
+	{
+		AccumulatedTime += DeltaSeconds;
+		return;
+	}
+	
+	if (static_cast<EBossState>(OwnerComp.GetBlackboardComponent()->GetValueAsEnum(BossStateKey)) == EBossState::Investigate)
+	{
+		LOG(TEXT("Chasing Finished !"))
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+		return;
+	}
+
+	
+}
