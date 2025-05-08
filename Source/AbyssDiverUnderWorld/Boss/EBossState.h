af2c25bf542fd16d34108670373acfe7369d8512
meo-young <meoyoung000@naver.com>
2025-05-08 15:10:30 +0900

    [Add] 보스 상태 Enum 추가

---
+++ Source/AbyssDiverUnderWorld/Boss/EBossState.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EBossState.generated.h"
+
+UENUM(BlueprintType)
+enum class EBossState : uint8
+{
+	Idle = 0 UMETA(DisplayName = "Idle"),
+	Move = 1 UMETA(DisplayName = "Move"),
+	Attack = 2 UMETA(DisplayName = "Attack"),
+};
