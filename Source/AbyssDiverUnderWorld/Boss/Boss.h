d3df9c326f162062b5fd66f4515ca13e95c8866b
meo-young <meoyoung000@naver.com>
2025-05-08 19:25:49 +0900

    [Update] 보스 추적 부모 함수 구현

--- Source/AbyssDiverUnderWorld/Boss/Boss.h
+++ Source/AbyssDiverUnderWorld/Boss/Boss.h
@@ -23,24 +23,33 @@ public:
 
 	/** 보스의 움직임을 정지하고 Idle Animation 재생 */
 	virtual void MoveStop();
+
+	/** 보스가 감지한 타겟을 추적하는 함수 */
+	virtual void MoveToTarget();
+
+	/** 보스가 마지막으로 감지한 타겟의 위치를 추적하는 함수 */
+	virtual void MoveToLastDetectedLocation();
+
+	/** 보스의 이동속도를 설정하는 함수 */
+	UFUNCTION(BlueprintImplementableEvent)
+	void SetMoveSpeed(float Speed);
 	
-protected:
 	UFUNCTION(NetMulticast, Reliable)
 	void M_PlayAnimation(UAnimMontage* AnimMontage, float InPlayRate = 1, FName StartSectionName = NAME_None);
 	void M_PlayAnimation_Implementation(UAnimMontage* AnimMontage, float InPlayRate = 1, FName StartSectionName = NAME_None);
+	
+protected:
 private:
 	
 #pragma endregion
 
 #pragma region Variable
 public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Boss|Target")
+	TObjectPtr<APawn> TargetPlayer;
 
-protected:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Boss|Blackboard")
-	TObjectPtr<UBlackboardComponent> BlackboardComponent;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Boss|AI")
-	TObjectPtr<ABossAIController> AIController;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Boss|Target")
+	FVector LastDetectedLocation;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Boss|Animation")
 	TObjectPtr<UAnimMontage> IdleAnimation;
@@ -53,6 +62,13 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Boss|Animation")
 	TArray<TObjectPtr<UAnimMontage>> SpecialAttackAnimations;
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Boss|Blackboard")
+	TObjectPtr<UBlackboardComponent> BlackboardComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Boss|AI")
+	TObjectPtr<ABossAIController> AIController;
 
 private:
 	
@@ -60,6 +76,9 @@ private:
 
 #pragma region Getter, Setter
 public:
+	APawn* GetTarget();
+	void SetTarget(APawn* Target);
+	void SetLastDetectedLocation(const FVector& InLastDetectedLocation);
 
 #pragma endregion
 	
