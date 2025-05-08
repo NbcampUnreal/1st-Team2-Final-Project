424373c9266c8920946b5bec9642a0d75f4dbbc1
swehio <63448248+swehio@users.noreply.github.com>
2025-05-08 20:47:07 +0900

    🎉 add ADInventoryComponent, FADItemDataRowLV_HGY ✨ updateFStructContainer, AbyssDiverUnderWorld.Build
    -ADInventoryComponent 생성 및 데이터 추가 코드 구현
    -아이템 데이터테이블 행 클래스 생성
    -아이템 구조체 FastArray 추가
    -빌드 파일에 UI와 FastArray를 위한 모듈 추가

--- Source/AbyssDiverUnderWorld/Container/FStructContainer.h
+++ Source/AbyssDiverUnderWorld/Container/FStructContainer.h
@@ -1,10 +1,12 @@
-﻿#pragma once
+#pragma once
 
 #include "CoreMinimal.h"
+#include "Net/Serialization/FastArraySerializer.h"
+#include "DataRow/FADItemDataRow.h"
 #include "FStructContainer.generated.h"
 
 USTRUCT(BlueprintType)
-struct FItemData
+struct FItemData : public FFastArraySerializerItem
 {
     GENERATED_BODY()
 
@@ -12,14 +14,84 @@ struct FItemData
     FName Name;
 
     UPROPERTY(EditAnywhere) 
-    uint8 Id;
+	uint8 Id;
 
     UPROPERTY(EditAnywhere) 
-    uint8 Quantity;
+	uint8 Quantity;
 
     UPROPERTY(EditAnywhere) 
-    int32 Amount;
+	int32 Amount;
 
     UPROPERTY(EditAnywhere)
     float Mass;
+
+	UPROPERTY(EditAnywhere)
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UTexture2D> Thumbnail;
+
+	FItemData()
+		: Name(NAME_None), Id(0), Quantity(0), Amount(0), ItemType(EItemType::Max), Thumbnail(nullptr)
+	{
+	}
+	FItemData(FName InName, uint8 InId, uint8 InQuantity, int32 InAmount, EItemType InType, UTexture2D* InThumbnail)
+		: Name(InName), Id(InId), Quantity(InQuantity), Amount(InAmount), ItemType(InType), Thumbnail(InThumbnail)
+	{
+	}
+
+	// 이 값이 바뀌면 Replication에 포함됨
+	bool operator==(const FItemData& Other) const
+	{
+		return Name == Other.Name && Quantity == Other.Quantity && ItemType == Other.ItemType && Thumbnail == Other.Thumbnail && Amount == Other.Amount;
+	}
+};
+
+USTRUCT()
+struct FInventoryList : public FFastArraySerializer
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TArray<FItemData> Items;
+
+	//Replication을 위한 필수 함수
+	bool NetDeltaSerialize(FNetDeltaSerializeInfo& DeltaParms)
+	{
+		return FFastArraySerializer::FastArrayDeltaSerialize<FItemData, FInventoryList>(Items, DeltaParms, *this);
+	}
+
+	void AddItem(const FItemData& Item)
+	{
+		FItemData NewItem;
+		NewItem.Name = Item.Name;
+		NewItem.Quantity = Item.Quantity;
+		NewItem.ItemType = Item.ItemType;
+		NewItem.Thumbnail = Item.Thumbnail;
+		NewItem.Amount = Item.Amount;
+		Items.Add(NewItem);
+		MarkArrayDirty();
+	}
+
+	void RemoveItem(int8 Index)
+	{
+		if (Items.IsValidIndex(Index))
+		{
+			Items.RemoveAt(Index);
+			MarkArrayDirty();
+		}
+	}
+
+	void Resize(int8 NewSize)
+	{
+		Items.SetNum(NewSize);
+		MarkArrayDirty();
+	}
+};
+
+// FInventoryList의 구조체를 등록하기 위한 매크로
+template<>
+struct TStructOpsTypeTraits<FInventoryList> : public TStructOpsTypeTraitsBase2<FInventoryList>
+{
+	enum { WithNetDeltaSerializer = true };
 };
---
+++ Source/AbyssDiverUnderWorld/Inventory/ADInventoryComponent.h
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Container/FStructContainer.h"
+#include "Net/Serialization/FastArraySerializer.h"
+#include "ADInventoryComponent.generated.h"
+
+
+enum class EItemType : uint8;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FInventoryUpdateDelegate, EItemType);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ABYSSDIVERUNDERWORLD_API UADInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UADInventoryComponent();
+protected:
+	virtual void BeginPlay() override;
+
+#pragma region Method
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override; //FastArray 관련 함수
+
+	UFUNCTION(BlueprintCallable)
+	void AddInventoryItem(FName Name, uint8 Count);
+
+	//*Remove 테스트 후 넣어야 함
+	//*Wiget 관련 함수 넣어야 함
+
+private:
+	int8 GetTypeInventoryEmptyIndex(EItemType ItemType);
+	int16 FindItemIndexById(FName ItemID);
+	void PrintLogInventoryData();
+
+	FInventoryUpdateDelegate InventoryUpdateDelegate;
+#pragma endregion
+	
+#pragma region Variable
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UDataTable> ItemDataTable; //*테스트용 추후 삭제
+	//*Wiget 관련 변수 넣어야 함
+
+private:
+	UPROPERTY(Replicated)
+	FInventoryList InventoryList;
+
+	int16 TotalWeight;
+	int16 WeightMax;
+	uint8 bInventoryWidgetShowed : 1;
+	TMap<EItemType, TArray<int8>> InventoryIndexMapByType;
+	TArray<int8> InventorySizeByType;
+#pragma endregion
+
+
+#pragma region Getter, Setter
+public:
+	int16 GetTotalWeight() const { return TotalWeight; }
+	const FInventoryList& GetInventoryList() { return InventoryList; }
+	const TArray<int8>& GetInventoryIndexesByType(EItemType ItemType) const { return InventoryIndexMapByType[ItemType]; }
+	const TArray<int8>& GetInventorySizeByType() const { return InventorySizeByType; }
+#pragma endregion
+};
