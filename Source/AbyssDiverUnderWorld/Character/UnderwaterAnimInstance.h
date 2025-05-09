3a8b992c6b8ce273570ff84fe4331c0aae16ca7e
handakim25 <handakim25@gmail.com>
2025-05-09 17:03:38 +0900

    Add: Underwater Anim Instance 추가
    - 추후에 C++ 단에서 작업할 수 있기 때문에 미리 추가

---
+++ Source/AbyssDiverUnderWorld/Character/UnderwaterAnimInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "UnderwaterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABYSSDIVERUNDERWORLD_API UUnderwaterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
