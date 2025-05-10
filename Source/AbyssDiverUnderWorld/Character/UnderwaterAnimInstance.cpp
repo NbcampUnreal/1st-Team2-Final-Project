1467efdc34ebf7b6a22bb9dffaf07e9a995d86a3
handakim25 <handakim25@gmail.com>
2025-05-10 23:59:36 +0900

    Add: AnimGraphRuntime Module Import, UnderwaterAnimInstance 생성

--- Source/AbyssDiverUnderWorld/Character/UnderwaterAnimInstance.cpp
+++ Source/AbyssDiverUnderWorld/Character/UnderwaterAnimInstance.cpp
@@ -2,3 +2,63 @@
 
 
 #include "UnderwaterAnimInstance.h"
+
+#include "UnderwaterCharacter.h"
+#include "KismetAnimationLibrary.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UUnderwaterAnimInstance::UUnderwaterAnimInstance()
+{
+	ShouldMoveThresholdSpeed = 3.0f;
+	
+	Speed = 0.0f;
+	ForwardSpeed = 0.0f;
+	RightSpeed = 0.0f;
+	UpSpeed = 0.0f;
+	Direction = 0.0f;
+	
+	bShouldMove = false;
+}
+
+void UUnderwaterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	UnderwaterCharacter = Cast<AUnderwaterCharacter>(TryGetPawnOwner());
+	if (UnderwaterCharacter.IsValid())
+	{
+		CharacterMovement = UnderwaterCharacter->GetCharacterMovement();
+	}
+}
+
+void UUnderwaterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!UnderwaterCharacter.IsValid() || !CharacterMovement.IsValid())
+	{
+		return;
+	}
+	
+	UpdateVariables();	
+}
+
+void UUnderwaterAnimInstance::UpdateVariables()
+{
+	const FVector Velocity = UnderwaterCharacter->GetVelocity();
+	Speed = Velocity.Size();
+
+	// 최소 속도 이상이고 가속도가 0 이상일 경우에 이동 모션을 출력한다.
+	// 가속도가 0일 경우고 속도가 0 이상일 경우는 밀리고 있는 상황이다.
+	const FVector Accel = CharacterMovement->GetCurrentAcceleration();
+	bShouldMove = Speed > ShouldMoveThresholdSpeed && !Accel.IsNearlyZero();
+
+	ForwardSpeed = FVector::DotProduct(UnderwaterCharacter->GetActorForwardVector(), Velocity);
+	RightSpeed = FVector::DotProduct(UnderwaterCharacter->GetActorRightVector(), Velocity);
+	UpSpeed = FVector::DotProduct(UnderwaterCharacter->GetActorUpVector(), Velocity);
+
+	// Actor는 Z축 기준으로만 회전하기 때문에 Rotation으로만 계산해도 현재로서는 문제 없다.
+	Direction = UKismetAnimationLibrary::CalculateDirection(Velocity, UnderwaterCharacter->GetActorRotation());
+
+	UE_LOG(LogTemp,Display, TEXT("Speed : %f, ForwardSpeed : %f, RightSpeed : %f, UpSpeed : %f, Direction : %f"), Speed, ForwardSpeed, RightSpeed, UpSpeed, Direction);
+}
