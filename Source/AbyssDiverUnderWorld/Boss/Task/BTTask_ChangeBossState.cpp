222447c5fe52d8c884a524490e01e3f85b807b50
meo-young <meoyoung000@naver.com>
2025-05-13 18:21:01 +0900

    [Update] 각 Task 별 상태 리플리케이션 적용 및 로그 추가

--- Source/AbyssDiverUnderWorld/Boss/Task/BTTask_ChangeBossState.cpp
+++ Source/AbyssDiverUnderWorld/Boss/Task/BTTask_ChangeBossState.cpp
@@ -1,5 +1,7 @@
 #include "Boss/Task/BTTask_ChangeBossState.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Boss/Boss.h"
+#include "Boss/BossAIController.h"
 #include "Boss/EBossState.h"
 
 const FName UBTTask_ChangeBossState::BossStateKey = "BossState";
@@ -12,7 +14,17 @@ UBTTask_ChangeBossState::UBTTask_ChangeBossState()
 
 EBTNodeResult::Type UBTTask_ChangeBossState::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-    OwnerComp.GetBlackboardComponent()->SetValueAsEnum(BossStateKey, static_cast<uint8>(BossState));
+	// 컨트롤러 캐스팅 실패 시 얼리 리턴
+	ABossAIController* AIController = Cast<ABossAIController>(OwnerComp.GetAIOwner());
+	if (!IsValid(AIController)) return EBTNodeResult::Failed;
 
+	// 보스 캐스팅에 실패하면 얼리 리턴
+	ABoss* Boss = Cast<ABoss>(AIController->GetCharacter());
+	if (!IsValid(Boss)) return EBTNodeResult::Failed;
+
+	// 지정한 상태로 보스의 상태 전이
+	Boss->SetBossState(BossState);
+
+	// 결과 성공 반환
 	return EBTNodeResult::Succeeded;
 }
