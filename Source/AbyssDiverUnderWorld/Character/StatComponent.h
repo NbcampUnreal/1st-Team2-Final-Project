e58c05c8c492a45cb05a24e5c59245459873e960
handakim25 <handakim25@gmail.com>
2025-05-15 16:53:31 +0900

    Update: Health Regen 기능 추가

--- Source/AbyssDiverUnderWorld/Character/StatComponent.h
+++ Source/AbyssDiverUnderWorld/Character/StatComponent.h
@@ -34,6 +34,13 @@ protected:
 	
 	UFUNCTION()
 	void OnRep_CurrentHealth();
+
+	virtual void RegenHealth();
+
+private:
+	void StopHealthRegen();
+
+	bool IsHealthRegenActive() const;
 	
 #pragma endregion 
 	
@@ -60,11 +67,26 @@ public:
 	/** 공격력 */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Stat")
 	int32 AttackPower;
+
+private:
+	/** 체력 회복 Timer */
+	FTimerHandle HealthRegenTimerHandle;
 	
+	/** 초당 체력 회복 속도 */
+	float HealthRegenRate;
+
+	/** 체력 회복 타이머 동작 주기 */
+	float HealthUpdateInterval;
+	
+	/** 체력 회복 후에 남은 소수점 값 */
+	float HealthAccumulator;
+
 #pragma endregion
 
 #pragma region Getter Setter
 
+public:
+	
 	/** 현재 최대 체력을 반환 */
 	FORCEINLINE int32 GetMaxHealth() const { return MaxHealth; }
 
@@ -76,6 +98,20 @@ public:
 
 	/** 공격력을 반환 */
 	FORCEINLINE int32 GetAttackPower() const { return AttackPower; }
+
+	/** 무분별하게 Health Regen Rate에 접근하고 수정을 하면 값을 잃어버릴 수 있다.
+	 * Regen Rate에 접근하는 객체가 많아지면 구조를 변경해야 한다.
+	 * 만약에 여러 객체에서 접근을 하게 된다면 Modifier를 정의해서 Modifer를 적용 / 비적용 상태로 구현한다.
+	 */
+
+	/** 초당 회복량을 반환 */
+	FORCEINLINE float GetHealthRegenRate() const { return HealthRegenRate; }
+
+	/** 초당 회복량을 설정 */
+	void SetHealthRegenRate(float NewHealthRegenRate);
+
+	/** 초당 회복량을 추가 */
+	void AddHealthRegenRate(float AddHealthRegenRate);
 	
 #pragma endregion
 };
