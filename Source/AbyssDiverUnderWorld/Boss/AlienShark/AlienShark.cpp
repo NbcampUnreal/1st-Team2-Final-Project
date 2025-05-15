eb95095661fa6bb4d3be5e5c870c6012326d54a5
meo-young <meoyoung000@naver.com>
2025-05-15 19:34:01 +0900

    [Add] AlienShark 클래스 기초 구현

---
+++ Source/AbyssDiverUnderWorld/Boss/AlienShark/AlienShark.cpp
@@ -0,0 +1,19 @@
+#include "Boss/AlienShark/AlienShark.h"
+#include "Components/CapsuleComponent.h"
+
+AAlienShark::AAlienShark()
+{
+	BiteCollision = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Bite Collision"));
+	BiteCollision->SetupAttachment(GetMesh(), TEXT("BiteSocket"));
+	BiteCollision->SetCapsuleHalfHeight(80.0f);
+	BiteCollision->SetCapsuleRadius(80.0f);
+	BiteCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	BiteCollision->ComponentTags.Add(TEXT("Bite Collision"));
+}
+
+void AAlienShark::BeginPlay()
+{
+	Super::BeginPlay();
+
+	BiteCollision->OnComponentBeginOverlap.AddDynamic(this, &ABoss::OnMeshOverlapBegin);
+}
