c0e8455bb4bbafd55995e53e7fd08d27e2c627a2
swehio <63448248+swehio@users.noreply.github.com>
2025-05-17 13:05:21 +0900

    🎉 add SpearGunBullet 및 테스트용 액터 추가

---
+++ Source/AbyssDiverUnderWorld/Projectile/ADSpearGunBullet.cpp
@@ -0,0 +1,156 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile/ADSpearGunBullet.h"
+#include "Kismet/GameplayStatics.h"
+#include "AbyssDiverUnderWorld.h"
+#include "Components/StaticMeshComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/EngineTypes.h"  
+#include "GameFramework/ProjectileMovementComponent.h"
+
+AADSpearGunBullet::AADSpearGunBullet() : BulletType(ESpearGunType::Basic), AdditionalDamage(0), PoisonDuration(0), bWasHit(false)
+{
+    StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SkeletalMesh"));
+    StaticMesh->SetupAttachment(RootComponent);
+    StaticMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision); 
+
+    Damage = 10.0f; //추후 450.0f
+}
+
+void AADSpearGunBullet::OnRep_BulletType()
+{
+    switch (BulletType)
+    {
+    case ESpearGunType::Basic:
+        break;
+    case ESpearGunType::Bomb:
+        AdditionalDamage = 20.0f;//200.0f;
+        break;
+    case ESpearGunType::Poison:
+        AdditionalDamage = 30.0f;//750.0f;
+        PoisonDuration = 4;
+        break;
+    default:
+        break;
+    }
+}
+
+void AADSpearGunBullet::BeginPlay()
+{
+    Super::BeginPlay();
+    LOG(TEXT("UpdatedComponent is %s"),ProjectileMovementComp->UpdatedComponent ? *ProjectileMovementComp->UpdatedComponent->GetName() : TEXT("NULL"));
+
+}
+
+void AADSpearGunBullet::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (!bWasHit)
+    {
+        if (OtherActor && OtherActor != this && OtherComp && OtherComp->GetOwner() != this)
+        {
+            UGameplayStatics::ApplyPointDamage(
+                OtherActor,
+                Damage,
+                GetActorForwardVector(),
+                SweepResult,
+                GetInstigatorController(),
+                this,
+                UDamageType::StaticClass()
+            );
+            LOG(TEXT("Hit %s"), *OtherActor->GetName());
+
+            ApplyAdditionalDamage();
+        }
+        ProjectileMovementComp->StopMovementImmediately();
+        ProjectileMovementComp->Deactivate();
+        bWasHit = true;
+    }
+}
+
+void AADSpearGunBullet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    DOREPLIFETIME(AADSpearGunBullet, BulletType);
+}
+
+void AADSpearGunBullet::ApplyAdditionalDamage()
+{
+    FTimerHandle BurstTimerHandle;
+    switch (BulletType)
+    {
+    case ESpearGunType::Basic:
+        LOG(TEXT("BasicBullet"));
+        break;
+    case ESpearGunType::Bomb:
+        //TODO : 삐삐삐 소리
+        LOG(TEXT("BombTimer"));
+        GetWorld()->GetTimerManager().SetTimer(BurstTimerHandle, this, &AADSpearGunBullet::Burst, 2.5f, false);
+        
+        break;
+    case ESpearGunType::Poison:
+        Addict();
+        break;
+    default:
+        break;
+    }
+}
+
+void AADSpearGunBullet::Burst()
+{
+    //TODO : 터지는 이펙트
+    LOG(TEXT("BombBullet"));
+
+    TArray<FHitResult> HitResults;
+    TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+    ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_WorldDynamic));
+
+    FVector Start = GetActorLocation();
+    FVector End = Start;
+    float Radius = 200.0f;
+
+    TArray<AActor*> IgnoreActors;
+    IgnoreActors.Add(this);
+
+    bool bHit = UKismetSystemLibrary::SphereTraceMultiForObjects(
+        GetWorld(),
+        Start,
+        End,
+        Radius,
+        ObjectTypes,
+        false,
+        IgnoreActors, // 무시할 액터
+        EDrawDebugTrace::ForDuration,
+        HitResults,
+        true
+    );
+    TSet<AActor*> UniqueActors;
+    if (bHit)
+    {
+        for (const FHitResult& Hit : HitResults)
+        {
+            if (AActor* HitActor = Hit.GetActor())
+            {
+                if (!UniqueActors.Contains(HitActor))
+                {
+                    UniqueActors.Add(HitActor);
+
+                    LOGN(TEXT("Unique hit: %s"), *HitActor->GetName());
+
+                    // 여기에 데미지 주기나 처리 로직 작성
+                }
+            }
+        }
+    }
+    else 
+    {
+        LOG(TEXT("No Hit in Bomb Area"));
+    }
+}
+
+void AADSpearGunBullet::Addict()
+{
+    //TODO : 디버프 컴포넌트 함수 호출
+    LOG(TEXT("PoisonBullet"));
+}
+
---
+++ Source/AbyssDiverUnderWorld/Projectile/ADSpearGunBullet.h
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile/ADProjectileBase.h"
+#include "ADSpearGunBullet.generated.h"
+
+UENUM(BlueprintType)
+enum class ESpearGunType : uint8
+{
+	Basic,
+	Bomb,
+	Poison
+};
+
+UCLASS()
+class ABYSSDIVERUNDERWORLD_API AADSpearGunBullet : public AADProjectileBase
+{
+	GENERATED_BODY()
+
+	AADSpearGunBullet();
+
+#pragma region Method
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult& SweepResult) override;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION()
+	void OnRep_BulletType();
+
+private:
+	void ApplyAdditionalDamage();
+
+	void Burst();
+
+	void Addict();
+#pragma endregion
+
+#pragma region Variable
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+private:
+	UPROPERTY(ReplicatedUsing = OnRep_BulletType, EditAnywhere, Category = "Bullet")
+	ESpearGunType BulletType;
+	UPROPERTY(VisibleAnywhere, Category = "Bullet")
+	int8 AdditionalDamage;
+	UPROPERTY(VisibleAnywhere, Category = "Bullet")
+	int8 PoisonDuration;
+
+	uint8 bWasHit : 1;
+
+#pragma endregion
+
+#pragma region Getter/Setter
+private:
+	//프로젝타일 스폰 후 지정
+	UFUNCTION(BlueprintCallable)
+	void SetBulletType(ESpearGunType NewBulletType) { BulletType = NewBulletType; }; 
+
+#pragma endregion
+	
+};
