be8445b927c438fee7fb6e3a1d46c2f23a98d3e7
JungHyaechang <hch9097@gmail.com>
2025-05-23 11:07:16 +0900

    [Add] BTService_CheckTargetDistance 추가 및 구현

---
+++ Source/AbyssDiverUnderWorld/Monster/BT/BTService_CheckTargetDistance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "BTService_CheckTargetDistance.generated.h"
+
+
+UCLASS()
+class ABYSSDIVERUNDERWORLD_API UBTService_CheckTargetDistance : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+	
+public:
+	UBTService_CheckTargetDistance();
+
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+#pragma region Varible
+public:
+	UPROPERTY(EditAnywhere, Category = "AI")
+	float MeleeRange = 200.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector InMeleeRangeKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector TargetActorKey;
+};
