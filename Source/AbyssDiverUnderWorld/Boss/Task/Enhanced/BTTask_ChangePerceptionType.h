606774151c2ee085cca7d1184fc6a19e3f2ec801
meo-young <meoyoung000@naver.com>
2025-05-26 15:58:50 +0900

    [Update] Task 역할 변경

--- Source/AbyssDiverUnderWorld/Boss/Task/Enhanced/BTTask_ChangePerceptionType.h
+++ Source/AbyssDiverUnderWorld/Boss/Task/Enhanced/BTTask_ChangePerceptionType.h
@@ -3,19 +3,18 @@
 #include "CoreMinimal.h"
 #include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
 #include "Boss/EnhancedBossAIController.h"
-#include "BTTask_HiddenPlayerChase.generated.h"
+#include "BTTask_ChangePerceptionType.generated.h"
 
 UCLASS()
-class ABYSSDIVERUNDERWORLD_API UBTTask_HiddenPlayerChase : public UBTTask_BlackboardBase
+class ABYSSDIVERUNDERWORLD_API UBTTask_ChangePerceptionType : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
 
 public:
-	UBTTask_HiddenPlayerChase();
+	UBTTask_ChangePerceptionType();
 
 private:
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& Comp, uint8* NodeMemory) override;
-	virtual void TickTask(UBehaviorTreeComponent& Comp, uint8* NodeMemory, float DeltaSeconds) override;
 
 private:
 	UPROPERTY()
@@ -23,5 +22,8 @@ private:
 
 	UPROPERTY()
 	TObjectPtr<AEnhancedBossAIController> AIController;
+
+	UPROPERTY(EditAnywhere)
+	EPerceptionType PerceptionType;
 	
 };
