adbd1c16ce2ff322f4e0a4448c30cf1f1c2c1710
meo-young <meoyoung000@naver.com>
2025-05-29 17:33:41 +0900

    [Add] Mini, Big Serpmare SpawnPoint 추가

---
+++ Source/AbyssDiverUnderWorld/Gimmic/Spawn/SpawnPoint/Serpmare/BigSerpmareSpawnPoint.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gimmic/Spawn/SpawnPoint/Monster/MonsterSpawnPoint.h"
+#include "BigSerpmareSpawnPoint.generated.h"
+
+UCLASS()
+class ABYSSDIVERUNDERWORLD_API ABigSerpmareSpawnPoint : public AMonsterSpawnPoint
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/AbyssDiverUnderWorld/Gimmic/Spawn/SpawnPoint/Serpmare/MiniSerpmareSpawnPoint.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gimmic/Spawn/SpawnPoint/Monster/MonsterSpawnPoint.h"
+#include "MiniSerpmareSpawnPoint.generated.h"
+
+UCLASS()
+class ABYSSDIVERUNDERWORLD_API AMiniSerpmareSpawnPoint : public AMonsterSpawnPoint
+{
+	GENERATED_BODY()
+	
+};
