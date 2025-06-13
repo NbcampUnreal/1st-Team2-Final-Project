9003343429f0e9a949606a646f9c891076023ba2
erldfd <hseongrae@gmail.com>
2025-06-13 14:43:41 +0900

    🐛 bugfix 캠프를 거치지 않고 게임 실행시 ResultScreen에서 크래시 나는 버그 및 기타
    ### 버그 수정
    - CampGameMode에서 PlayerId, Index를 등록해주는데 그 과정이 없어서 버그났었음..
    
    ### 기타
    - 플레이어 인덱스 1 기반에서 0기반으로 변경

--- Source/AbyssDiverUnderWorld/Framework/ADInGameMode.cpp
+++ Source/AbyssDiverUnderWorld/Framework/ADInGameMode.cpp
@@ -207,6 +207,28 @@ void AADInGameMode::InitPlayer(APlayerController* PC)
 	{
 		RestartPlayer(PC);
 	}
+
+	FString NewPlayerId = PC->GetPlayerState<AADPlayerState>()->GetUniqueId()->ToString();
+
+	LOGV(Warning, TEXT("%s Has Entered"), *NewPlayerId);
+	UADGameInstance* GI = GetGameInstance<UADGameInstance>();
+	check(GI);
+
+	if (AADPlayerState* ADPlayerState = PC->GetPlayerState<AADPlayerState>())
+	{
+		ADPlayerState->ResetLevelResults();
+		GI->AddPlayerNetId(NewPlayerId);
+
+		int32 NewPlayerIndex = INDEX_NONE;
+		if (GI->TryGetPlayerIndex(NewPlayerId, NewPlayerIndex) == false)
+		{
+			LOGV(Error, TEXT("Fail To Get Player Index"));
+			return;
+		}
+
+		ADPlayerState->SetPlayerNickname(NewPlayerId);
+		ADPlayerState->SetPlayerIndex(NewPlayerIndex);
+	}
 }
 
 void AADInGameMode::GameOver()
