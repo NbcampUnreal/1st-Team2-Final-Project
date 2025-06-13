2281bdf31f5b6829fcf9f3d0903d7045a8628324
meo-young <meoyoung000@naver.com>
2025-06-13 12:37:34 +0900

    [Add] BattleFieldVolume
    - 보스의 전투 영역을 지정할 수 있는 액터

---
+++ Source/AbyssDiverUnderWorld/Gimmic/Volume/BattleFieldVolume.cpp
@@ -0,0 +1,51 @@
+#include "Gimmic/Volume/BattleFieldVolume.h"
+#include "Character/UnderwaterCharacter.h"
+
+void ABattleFieldVolume::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 필드에 플레이어가 남아있는 지 확인하기 위해 함수를 호출할 때마다 Overlap을 하여 확인하는 방법도 있다.
+	// Volume의 크기가 커짐에 따라 비용이 증가할 우려가 있기 때문에
+	// 플레이어가 오버랩 될 때마다 목록에 추가하는 방식으로 관리한다.
+	OnActorBeginOverlap.AddDynamic(this, &ABattleFieldVolume::OnComponentBeginOverlap);
+	OnActorEndOverlap.AddDynamic(this, &ABattleFieldVolume::OnComponentEndOverlap);
+}
+
+AUnderwaterCharacter* ABattleFieldVolume::GetBattleFieldPlayer()
+{
+	// 배열이 비어있는지 확인
+	if (BattleFieldPlayers.Num() == 0) return nullptr;
+	
+	// 랜덤 인덱스 생성 (0부터 배열 크기-1까지)
+	const uint8 RandomIndex = FMath::RandRange(0, BattleFieldPlayers.Num() - 1);
+    
+	// 랜덤으로 선택된 플레이어 반환
+	return BattleFieldPlayers[RandomIndex].Get();
+}
+
+void ABattleFieldVolume::OnComponentBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	AUnderwaterCharacter* Player = Cast<AUnderwaterCharacter>(OverlappedActor);
+	if (!IsValid(Player)) return;
+
+	if (BattleFieldPlayers.Contains(Player)) return;
+	
+	BattleFieldPlayers.Add(Player);
+}
+
+void ABattleFieldVolume::OnComponentEndOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	AUnderwaterCharacter* Player = Cast<AUnderwaterCharacter>(OverlappedActor);
+	if (!IsValid(Player)) return;
+
+	if (!BattleFieldPlayers.Contains(Player)) return;
+	
+	BattleFieldPlayers.Remove(Player);
+
+	if (OnBattleFieldEndOverlapDelegate.IsBound())
+	{
+		OnBattleFieldEndOverlapDelegate.Broadcast(BattleFieldPlayers.Num());
+	}
+	
+}
---
+++ Source/AbyssDiverUnderWorld/Gimmic/Volume/BattleFieldVolume.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "BattleFieldVolume.generated.h"
+
+class AUnderwaterCharacter;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnBattleFieldEndOverlap, uint8, BattlePlayerNum);
+
+UCLASS()
+class ABYSSDIVERUNDERWORLD_API ABattleFieldVolume : public ATriggerBox
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+public:
+	FOnBattleFieldEndOverlap OnBattleFieldEndOverlapDelegate;
+
+public:
+	/** 현재 전투 필드에 남아있는 플레이어 중 랜덤으로 1명 반환하는 함수 */
+	AUnderwaterCharacter* GetBattleFieldPlayer();
+
+private:
+	UFUNCTION()
+	void OnComponentBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+
+	UFUNCTION()
+	void OnComponentEndOverlap(AActor* OverlappedActor, AActor* OtherActor);
+
+private:
+	/** 전투 필드에 남아있는 플레이어 목록 */
+	TArray<TObjectPtr<AUnderwaterCharacter>> BattleFieldPlayers;
+	
+};
