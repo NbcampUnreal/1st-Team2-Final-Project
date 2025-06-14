18bf62274c8dae0c6eae047cbe9a1c5aad2641a0
swehio <63448248+swehio@users.noreply.github.com>
2025-06-14 16:50:05 +0900

    Revert "saved UNDO information for reset changes and discarded files"
    This reverts commit 4fa7b742def4f7dbfd930fdb12b7ce5911f13587.

--- Source/AbyssDiverUnderWorld/Inventory/ADInventoryComponent.cpp
+++ Source/AbyssDiverUnderWorld/Inventory/ADInventoryComponent.cpp
@@ -375,7 +375,7 @@ bool UADInventoryComponent::AddInventoryItem(const FItemData& ItemData)
 {
 	if (TotalWeight + ItemData.Mass <= WeightMax)
 	{
-		FFADItemDataRow* FoundRow = DataTableSubsystem->GetItemData(ItemData.Id); 
+		FFADItemDataRow* FoundRow = DataTableSubsystem->GetItemDataByName(ItemData.Name); 
 		if (FoundRow)
 		{
 			int8 ItemIndex = FindItemIndexByName(ItemData.Name);
