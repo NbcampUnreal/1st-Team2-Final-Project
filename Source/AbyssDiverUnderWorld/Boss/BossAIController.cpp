d01697ba7b7c2063578be35e80aa25e9388caa8e
meo-young <meoyoung000@naver.com>
2025-06-17 14:12:19 +0900

    [Refactoring] BossAIController 리팩토링

--- Source/AbyssDiverUnderWorld/Boss/BossAIController.cpp
+++ Source/AbyssDiverUnderWorld/Boss/BossAIController.cpp
@@ -48,8 +48,6 @@ ABossAIController::ABossAIController()
 
 	AIPerceptionComponent->SetDominantSense(SightConfig->GetSenseImplementation());
 
-	bIsDetectedStatePossible = true;
-	AccumulatedTime = 0.0f;
 	DetectedStateInterval = 20.0f;
 
 	// 시야 감지 가능한 상태로 설정
@@ -59,11 +57,6 @@ ABossAIController::ABossAIController()
 void ABossAIController::BeginPlay()
 {
 	Super::BeginPlay();
-
-	if (IsValid(AIPerceptionComponent) && bIsSightDetectionPossible)
-	{
-		AIPerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &ABossAIController::OnTargetPerceptionUpdated);
-	}
 }
 
 
@@ -80,7 +73,21 @@ void ABossAIController::OnPossess(APawn* InPawn)
 	Boss = Cast<ABoss>(InPawn);
 }
 
-void ABossAIController::SetVisionAngle(float& Angle)
+void ABossAIController::SetSightRadius(float InRadius)
+{
+	if (!IsValid(AIPerceptionComponent)) return;
+    
+	UAISenseConfig_Sight* SightConfigInstance = Cast<UAISenseConfig_Sight>(
+		AIPerceptionComponent->GetSenseConfig(UAISense::GetSenseID(UAISense_Sight::StaticClass()))
+	);
+    
+	if (!SightConfigInstance) return;
+    
+	SightConfigInstance->SightRadius = InRadius;
+	AIPerceptionComponent->ConfigureSense(*SightConfigInstance);
+}
+
+void ABossAIController::SetVisionAngle(float Angle)
 {
 	// Angle 수치로 시야각 전환
 	UAISenseConfig_Sight* SightConfigInstance = Cast<UAISenseConfig_Sight>(AIPerceptionComponent->GetSenseConfig(UAISense::GetSenseID(UAISense_Sight::StaticClass())));
@@ -93,84 +100,12 @@ bool ABossAIController::IsStateSame(EBossState State)
 	return (GetBlackboardComponent()->GetValueAsEnum(BossStateKey) == static_cast<uint8>(State));
 }
 
-void ABossAIController::OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+EPathFollowingRequestResult::Type ABossAIController::MoveToActorWithRadius(AActor* TargetActor)
 {
-	// 감지한 대상이 플레이어가 아닌 경우 얼리 리턴
-	AUnderwaterCharacter* Player = Cast<AUnderwaterCharacter>(Actor);
-	if (!IsValid(Player)) return;
-
-	// 시각 자극인지 확인
-	if (Stimulus.Type != UAISense::GetSenseID<UAISense_Sight>()) return;
-	
-	// 감지에 성공 했다면 플레이어 감지 리스트에 추가
-	if (Stimulus.WasSuccessfullySensed())
-	{
-		M_AddDetectedPlayer(Player);
-	}
-	// 시야각에서 벗어났다면 플레이어 감지 리스트에서 제거
-	else
-	{
-		M_RemoveDetectedPlayer(Player);
-	}
-}
-
-void ABossAIController::M_AddDetectedPlayer_Implementation(AUnderwaterCharacter* Target)
-{
-	if (!IsValid(Target)) return;
-
-	// 감지된 플레이어 리스트에 추가
-	DetectedPlayers.Add(Target);
-	
-	// 이미 추적중인 플레이어가 있는 경우 얼리 리턴
-	if (IsValid(Boss->GetTarget()))
-	{
-		return;
-	}
-
-	// 최초로 플레이어를 감지한 경우 추적할 타겟으로 설정
-	Boss->SetTarget(Target);
-
-	// Detected 상태로 전이된 지 DetectedStateInterval이 지난 경우 Detected 상태로 전이
-	if (bIsDetectedStatePossible)
-	{
-		Boss->SetBossState(EBossState::Detected);
-		bIsDetectedStatePossible = false;
-		GetWorldTimerManager().SetTimer(DetectedStateTimerHandle, this, &ABossAIController::SetDetectedStatePossible, DetectedStateInterval, false);
-	}
-	// 만약 아니라면 Chase 상태로 전이
-	else
-	{
-		Boss->SetBossState(EBossState::Chase);
-	}
-}
-
-void ABossAIController::M_RemoveDetectedPlayer_Implementation(AUnderwaterCharacter* Target)
-{
-	if (!IsValid(Target)) return;
-	
-	// 감지된 플레이어 리스트에서 제거
-	DetectedPlayers.Remove(Target);
-
-	// 시야각에서 사라진 타겟이 추적중이었던 플레이어인 경우 Investigate 상태로 전이
-	if (Target == Boss->GetTarget())
-	{
-		Boss->SetLastDetectedLocation(Target->GetActorLocation());
-		Boss->SetBossState(EBossState::Investigate);
-		Boss->InitTarget();
-	}
-}
-
-EPathFollowingRequestResult::Type ABossAIController::MoveToActorWithRadius()
-{
-	return MoveToActor(Boss->GetTarget(), MoveToActorAcceptanceRadius);
+	return MoveToActor(TargetActor, MoveToActorAcceptanceRadius);
 }
 
 EPathFollowingRequestResult::Type ABossAIController::MoveToLocationWithRadius(const FVector& Location)
 {
 	return MoveToLocation(Location, MoveToLocationAcceptanceRadius);
-}
-
-void ABossAIController::SetDetectedStatePossible()
-{
-	bIsDetectedStatePossible = true;
 }
