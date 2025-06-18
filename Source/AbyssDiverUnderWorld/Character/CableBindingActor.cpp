e14759c5d74a227f3d2c56071cb29ae41a47cd68
handakim25 <handakim25@gmail.com>
2025-06-18 09:25:43 +0900

    Add: CableBindingActor 구현

---
+++ Source/AbyssDiverUnderWorld/Character/CableBindingActor.cpp
@@ -0,0 +1,110 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CableComponent.h"
+#include "CableBindingActor.h"
+
+#include "UnderwaterCharacter.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+
+
+// Sets default values
+ACableBindingActor::ACableBindingActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CableLength = 500.0f;
+	ConstraintStiffness = 1500.0f;
+	ConstraintDamping = 200.0f;
+
+	CableComponent = CreateDefaultSubobject<UCableComponent>(TEXT("CableComponent"));
+	SetRootComponent(CableComponent);
+	CableComponent->CableWidth = 5.0f;
+	CableComponent->NumSegments = 20;
+
+	PhysicsConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(TEXT("PhysicsConstraint"));
+	PhysicsConstraint->SetupAttachment(CableComponent);
+
+	SourceActor = nullptr;
+	TargetActor = nullptr;
+}
+
+void ACableBindingActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ACableBindingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (IsConnected())
+	{
+		UpdateCable();
+	}
+}
+
+void ACableBindingActor::ConnectActors(AActor* NewSourceActor, AActor* NewTargetActor)
+{
+	if (!NewSourceActor || !NewTargetActor)
+	{
+		UE_LOG(LogAbyssDiverCharacter, Warning, TEXT("ACableBindingActor::ConnectActors - Invalid actors provided."));
+		return;
+	}
+
+	CableComponent->SetAttachEndToComponent(NewTargetActor->GetRootComponent(), NAME_None);
+	CableComponent->EndLocation = FVector::ZeroVector;
+
+	UPrimitiveComponent* SourceComponent = Cast<UPrimitiveComponent>(NewSourceActor->GetRootComponent());
+	UPrimitiveComponent* TargetComponent = Cast<UPrimitiveComponent>(NewTargetActor->GetRootComponent());
+
+	if (SourceComponent && TargetComponent)
+	{
+		PhysicsConstraint->SetWorldLocation(SourceActor->GetActorLocation());
+		PhysicsConstraint->SetConstrainedComponents(SourceComponent, NAME_None, TargetComponent, NAME_None);
+
+		PhysicsConstraint->SetLinearXLimit(LCM_Limited, CableLength);
+		PhysicsConstraint->SetLinearYLimit(LCM_Limited, CableLength);
+		PhysicsConstraint->SetLinearZLimit(LCM_Limited, CableLength);
+
+		PhysicsConstraint->SetAngularSwing1Limit(ACM_Free, 0.0f);
+		PhysicsConstraint->SetAngularSwing2Limit(ACM_Free, 0.0f);
+		PhysicsConstraint->SetAngularTwistLimit(ACM_Free, 0.0f);
+
+		PhysicsConstraint->SetLinearPositionDrive(true, true, true);
+		PhysicsConstraint->SetLinearVelocityDrive(true, true, true);
+		PhysicsConstraint->SetLinearDriveParams(ConstraintStiffness, ConstraintDamping, 0.0f);
+	}
+	else
+	{
+		UE_LOG(LogAbyssDiverCharacter, Warning, TEXT("ACableBindingActor::ConnectActors - Actors do not have valid root components."));
+	}
+}
+
+void ACableBindingActor::DisconnectActors()
+{
+	PhysicsConstraint->BreakConstraint();
+
+	CableComponent->SetAttachEndTo(nullptr, NAME_None);
+
+	SourceActor = nullptr;
+	TargetActor = nullptr;
+}
+
+void ACableBindingActor::UpdateCable()
+{
+	if (!SourceActor || !TargetActor)
+	{
+		return;
+	}
+
+	SetActorLocation(SourceActor->GetActorLocation());
+	CableComponent->EndLocation = TargetActor->GetActorLocation() - SourceActor->GetActorLocation();
+
+	PhysicsConstraint->SetWorldLocation(SourceActor->GetActorLocation());
+}
+
+bool ACableBindingActor::IsConnected() const
+{
+	return SourceActor != nullptr && TargetActor != nullptr; 
+}
+
---
+++ Source/AbyssDiverUnderWorld/Character/CableBindingActor.h
@@ -0,0 +1,98 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CableBindingActor.generated.h"
+
+/*
+ * 2개의 Actor를 연결하는 Actor
+ * - Source Actor: Cable이 연결되어서 끌고 가는 Actor
+ * - Target Actor: Cable이 연결되어서 끌려가는 Actor
+ * Target Actor는 물리 시뮬레이션이 활성화되어 있어야 한다.
+ */
+UCLASS()
+class ABYSSDIVERUNDERWORLD_API ACableBindingActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACableBindingActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+#pragma region Method
+
+public:
+
+	/** Source Actor와 Target Actor를 연결한다.
+	 * Source Actor는 Cable의 시작점이 되고, Target Actor는 Cable의 끝점이 된다.
+	 * Target Actor는 물리 시뮬레이션이 활성화되어 있어야 한다.
+	 */
+	UFUNCTION(BlueprintCallable)
+	void ConnectActors(AActor* NewSourceActor, AActor* NewTargetActor);
+
+	/** Source Actor와 Target Actor를 분리한다.
+	 * Cable을 제거하고, Physics Constraint를 비활성화한다.
+	 */
+	UFUNCTION(BlueprintCallable)
+	void DisconnectActors();
+
+protected:
+
+	/** Cable이 연결되어 있는지 확인한다.
+	 * Source Actor와 Target Actor가 모두 유효한지 확인한다.
+	 */
+	void UpdateCable();
+	
+#pragma endregion
+
+#pragma region Variable
+
+protected:
+
+	/** Cable Component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<class UCableComponent> CableComponent;
+
+	/** 물리 제약을 위한 Physics Constraint Component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<class UPhysicsConstraintComponent> PhysicsConstraint;
+
+	/** Cable이 연결되는 Source Actor. Source Actor는 끌고가는 Actor이다.(Binder Actor) */
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<AActor> SourceActor;
+
+	/** Cable이 연결되는 Target Actor. Target Actor는 끌려가는 Actor이다.(Bound Actor) */
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<AActor> TargetActor;
+
+	/** Cable의 길이 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CableLength;
+
+	/** Constraint의 강성도 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ConstraintStiffness;
+
+	/** Constraint의 감쇠력 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ConstraintDamping;
+	
+#pragma endregion
+
+#pragma region Getter Setter
+
+public:
+
+	/** 현재 연결 상태를 반환 */
+	UFUNCTION(BlueprintCallable)
+	bool IsConnected() const;
+	
+#pragma endregion
+};
