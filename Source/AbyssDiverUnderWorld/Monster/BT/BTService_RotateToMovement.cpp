4e336973f9f84a7223679fc2f010fe2175c0baa9
JungHyaechang <hch9097@gmail.com>
2025-06-18 09:22:01 +0900

    [Fix] 호러크리처 삼킨 후 5초 후엔 무조건 뱉도록 로직 추가, BT 수정 / 장애물 있을 때 Monster Rotate 자연스럽게 수정,

---
+++ Source/AbyssDiverUnderWorld/Monster/BT/BTService_RotateToMovement.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Monster/BT/BTService_RotateToMovement.h"
+#include "AIController.h"
+#include "Monster/Monster.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UBTService_RotateToMovement::UBTService_RotateToMovement()
+{
+	NodeName = "Rotate To Movement Direction";
+	bNotifyTick = true;
+}
+
+void UBTService_RotateToMovement::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if (!AIController) return;
+
+	AMonster* Monster = Cast<AMonster>(AIController->GetPawn());
+	if (!Monster) return;
+
+	UCharacterMovementComponent* MovementComp = Monster->GetCharacterMovement();
+	if (!MovementComp) return;
+
+	// No rotation when stationary
+	const FVector MonsterVelocity = MovementComp->Velocity;
+	if (MonsterVelocity.SizeSquared() < KINDA_SMALL_NUMBER) return;
+
+	// Movement direction ¡æ Rotate
+	FRotator TargetRotation = MonsterVelocity.GetSafeNormal().Rotation();
+	TargetRotation.Roll = 0.0f;
+
+	// Interpolation
+	FRotator NewRotation = FMath::RInterpTo(Monster->GetActorRotation(), TargetRotation, DeltaSeconds, 5.0f);
+	Monster->SetActorRotation(NewRotation);
+}
