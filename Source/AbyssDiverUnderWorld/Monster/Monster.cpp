b6f837fb44aff08f17809c7360be7847bd1de2e3
JungHyaechang <hch9097@gmail.com>
2025-06-25 11:02:43 +0900

    [Fix] 광석, 몬스터 콜리전 수정, 호러크리처 크기 수정

--- Source/AbyssDiverUnderWorld/Monster/Monster.cpp
+++ Source/AbyssDiverUnderWorld/Monster/Monster.cpp
@@ -159,7 +159,7 @@ float AMonster::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, 
 			}
 			
 			// If aggro is not on TargetPlayer, set aggro
-			if (MonsterState != EMonsterState::Chase)
+			if (MonsterState != EMonsterState::Chase && MonsterState != EMonsterState::Flee)
 			{
 				AddDetection(InstigatorPlayer);
 				SetMonsterState(EMonsterState::Chase);
