686363ce96749e36cbb32ec45818019e83e0dfcf
JungHyaechang <hch9097@gmail.com>
2025-09-29 11:10:35 +0900

    [✨ Update, 🛠️Fix] Blowfish FX 추가, HitReat Animation 적용, ExplosionTrigger Speed 버그 수정
    - ✨ Hit_React Animation & Sound 추가
    - ✨ Explosion FX(Particle, Sound) 추가
    -  기존 상태별 speed 적용되는 로직 수정, StatComponent의 Movespeed * 상황별Multiplier 로 적용

--- Source/AbyssDiverUnderWorld/Monster/Blowfish/Blowfish.h
+++ Source/AbyssDiverUnderWorld/Monster/Blowfish/Blowfish.h
@@ -23,34 +23,44 @@ public:
 	void M_TriggerExplosion_Implementation();
 
 	void Explosion();
+	void ApplyExplosionEffect();
 
 protected:
 	UFUNCTION(BlueprintImplementableEvent)
 	void ScaleUpTriggered();
 	
 protected:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Boss|Stat")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Monster|Stat")
 	float ExplosionTriggeredMovementMultiplier;
 	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Boss|Stat")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Monster|Stat")
 	float ExplosionHealthRatio;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Boss|Stat")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Monster|Stat")
 	float ExplosionDelayTime;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Boss|Stat")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Monster|Stat")
 	float ExplosionRadius = 500.0f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Boss|Stat")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Monster|Stat")
 	float ExplosionDamage = 100.0f;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Boss|Collision")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Monster|Collision")
 	TObjectPtr<UCapsuleComponent> BiteCollision;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Boss|Collision")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Monster|Collision")
 	TObjectPtr<UCapsuleComponent> TailCollision;
 
 private:
 	uint8 bIsExplosionTriggered : 1;
 	FTimerHandle ExplosionTimerHandle;
+
+	UPROPERTY(EditAnywhere, Category = "AI|FX")
+	TObjectPtr<UNiagaraSystem> ExplosionEffect;
+
+	UPROPERTY(EditAnywhere, Category = "AI|FX")
+	TObjectPtr<USoundBase> ExplosionCountdownSound;
+
+	UPROPERTY(EditAnywhere, Category = "AI|FX")
+	TObjectPtr<USoundBase> ExplosionSound;
 };
