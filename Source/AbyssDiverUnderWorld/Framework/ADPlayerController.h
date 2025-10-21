39366c9239e3c7e19288fcaaa574d4ea82efd4fc
handakim25 <handakim25@gmail.com>
2025-10-21 18:06:45 +0900

    BugFix: 이동 중 Confirm Popup을 띄웠을 때 이동이 지속되는 현상 수정

--- Source/AbyssDiverUnderWorld/Framework/ADPlayerController.h
+++ Source/AbyssDiverUnderWorld/Framework/ADPlayerController.h
@@ -70,7 +70,7 @@ public:
 	 */
 	virtual void SetViewTarget(class AActor* NewViewTarget, FViewTargetTransitionParams TransitionParams = FViewTargetTransitionParams()) override;
 
-	/** Camera Blank를 시작한다. FadeAlpha의 X값은 시작 알파 값이고 Y값은 종료 알파 값이다.
+	/** Camera Blink를 시작한다. FadeAlpha의 X값은 시작 알파 값이고 Y값은 종료 알파 값이다.
 	 * FadeStartTime 동안 종료 알파 값으로 Fade Out을 한다.
 	 * FadeEndDelay 동안 대기한다.
 	 * FadeOut 완료되면 FadeEndTime 동안 FadeColor로 Fade In을 한다.
